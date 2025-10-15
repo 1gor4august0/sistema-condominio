3191bcbf13f9bb05e22bbb365cd09a5426710bd2
Aluno Lab. 08 <aluno08@aeelabs.edu.br>
2025-10-14 21:43:42 -0300

    chore: versão inicial antes das correções

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,11 +5,21 @@ namespace App\Http\Controllers;
 use App\Constants\Geral;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Credenciais inválidas',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'status' => true,
