d71e3243877aa944105d770f57b05fbe3ad22099
1gor4august0 <igoraugustorpmartins@gmail.com>
2025-10-17 22:17:06 -0300

    Adiciona validação obrigatória para nome do condomínio :wq

--- app/Http/Controllers/CondominioController.php
+++ app/Http/Controllers/CondominioController.php
@@ -20,44 +20,44 @@ class CondominioController extends Controller
     {
         $condominio = $this->service->create($request);
 
-        return ['status' => true, 'message' => Geral::CONDOMINIO_CADASTRADO, 'condominio' => $condominio];
+        return [
+            'status' => true,
+            'message' => Geral::CONDOMINIO_CADASTRADO,
+            'condominio' => $condominio
+        ];
     }
 
     public function list(Request $request)
     {
         $condominio = $this->service->list($request);
 
-        return ['status' => true, 'message' => Geral::CONDOMINIO_ENCONTRADO, 'condominio' => $condominio];
+        return [
+            'status' => true,
+            'message' => Geral::CONDOMINIO_ENCONTRADO,
+            'condominio' => $condominio
+        ];
     }
 
     public function search(Request $request)
     {
         $condominio = $this->service->search($request);
 
-        return ['status' => true, 'message' => Geral::CONDOMINIO_ENCONTRADO, 'condominio' => $condominio];
+        return [
+            'status' => true,
+            'message' => Geral::CONDOMINIO_ENCONTRADO,
+            'condominio' => $condominio
+        ];
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
     }
 }
